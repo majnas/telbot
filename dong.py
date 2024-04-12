deba81199287ff7d25b884fa875ef005299a2135
Majid Nasiri <majnasai@gmail.com>
2024-04-13 01:22:33 +1000

    set stat

--- dong.py
+++ dong.py
@@ -5,6 +5,8 @@ from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
                           ContextTypes, ConversationHandler, MessageHandler, filters)
 
 from icecream import ic
+from dataclasses import dataclass
+from typing import List
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,55 +14,88 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# Define states
-CAR_TYPE, CAR_COLOR, CAR_MILEAGE_DECISION, CAR_MILEAGE, PHOTO, SUMMARY = range(6)
+@dataclass
+class Team:
+    name: str
+    dad: str
+    mon: str
+    n: 2
+    addtext: str
+    minustext: str
+    icon: None
+    def __str__(self) -> str:
+        return f"{self.name} {self.n}"
 
+TEAMS: List[Team] = []
+TEAMS.append(Team("Majid", "Majid", "Safoura", 3, "Majid+", "Majid-", "🚗"))
+TEAMS.append(Team("Mammad", "Mammad", "Saba", 3, "Mammad+", "Mammad-",  "🚙"))
+TEAMS.append(Team("Hossein", "Hossein", "Parisa", 2, "Hossein+", "Hossein-","🏎️"))
+TEAMS.append(Team("Aref", "Aref", "Nafise", 2, "Aref+", "Aref-", "🚕"))
+TEAMS.append(Team("Masoud", "Masoud", "Mahshid", 2, "Masoud+", "Masoud-", "🚛"))
+
+def update_teams(text: str)-> None:
+    for team in TEAMS:
+        if text == team.addtext:
+            team.n += 1 
+        elif text == team.minustext:
+            team.n -= 1
+
+# Define states
+STATISTICS, CAR_COLOR, CAR_MILEAGE_DECISION, CAR_MILEAGE, PHOTO, SUMMARY = range(6)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Starts the conversation and asks the user about their preferred car type."""
-    reply_keyboard = [['Sedan', 'SUV', 'Sports', 'Electric'], 
-                      ['E'],
-                      ['S2', 'S', 'E'],
-                      ['S4', 'S'],
-                      ['S6', 'S8', 'E0'],
-                      ['See', 'Sbb', 'E22'],
-                      ]
+    reply_keyboard = [[t.minustext, str(t.n), t.addtext] for t in TEAMS]
+    reply_keyboard += [["Apply"]]
 
     await update.message.reply_text(
-        '<b>Welcome to the Car Sales Listing Bot!\n'
-        'Let\'s get some details about the car you\'re selling.\n'
-        'What is your car type?</b>',
+        '<b>Set statistics\n</b>',
         parse_mode='HTML',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True, is_persistent=True),
     )
 
-    return CAR_TYPE
+    return STATISTICS
 
 
-async def car_type(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the user's car type."""
-    user = update.message.from_user
-    context.user_data['car_type'] = update.message.text
-    cars = {"Sedan": "🚗", "SUV": "🚙", "Sports": "🏎️", "Electric": "⚡"}
-    logger.info('Car type of %s: %s', user.first_name, update.message.text)
-    await update.message.reply_text(
-        f'<b>You selected {update.message.text} car {cars[update.message.text]}.\n'
-        f'What color your car is?</b>',
-        parse_mode='HTML',
-        reply_markup=ReplyKeyboardRemove(),
-    )
+async def statistics(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Apply":
+        user = update.message.from_user
+        context.user_data['car_type'] = update.message.text
+        logger.info('Car type of %s: %s', user.first_name, update.message.text)
+        ic(update.message)
+        await update.message.reply_text(
+            f'<b>You selected {update.message.text} car {update.message.text}.\n'
+            f'What color your car is?</b>',
+            parse_mode='HTML',
+            reply_markup=ReplyKeyboardRemove(),
+        )
 
-    # Define inline buttons for car color selection
-    keyboard = [
-        [InlineKeyboardButton('Red', callback_data='Red')],
-        [InlineKeyboardButton('Blue', callback_data='Blue')],
-        [InlineKeyboardButton('Black', callback_data='Black')],
-        [InlineKeyboardButton('White', callback_data='White')],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('<b>Please choose:</b>', parse_mode='HTML', reply_markup=reply_markup)
+        # Define inline buttons for car color selection
+        keyboard = [
+            [InlineKeyboardButton('Red', callback_data='Red')],
+            [InlineKeyboardButton('Blue', callback_data='Blue')],
+            [InlineKeyboardButton('Black', callback_data='Black')],
+            [InlineKeyboardButton('White', callback_data='White')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('<b>Please choose:</b>', parse_mode='HTML', reply_markup=reply_markup)
+        return CAR_COLOR
+    else:
+        ic(update.message.text)
+        update_teams(text=update.message.text)
+        # print([ic(t) for t in TEAMS])
+        reply_keyboard = [[t.minustext, str(t.n), t.addtext] for t in TEAMS]
+        reply_keyboard += [["Apply"]]
 
-    return CAR_COLOR
+        await update.message.reply_text(
+            '<b>Set statistics\n</b>',
+            parse_mode='HTML',
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True, is_persistent=True),
+        )
+        return STATISTICS
+        
+
+
 
 
 async def car_color(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -193,7 +228,7 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            CAR_TYPE: [MessageHandler(filters.TEXT & ~filters.COMMAND, car_type)],
+            STATISTICS: [MessageHandler(filters.TEXT & ~filters.COMMAND, statistics)],
             CAR_COLOR: [CallbackQueryHandler(car_color)],
             CAR_MILEAGE_DECISION: [CallbackQueryHandler(car_mileage_decision)],
             CAR_MILEAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, car_mileage)],
